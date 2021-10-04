c1378c0c2c296ae907e26d1b258368c12e16470b
gabriellysanches <91857822+gabriellysanches@users.noreply.github.com>
2021-10-04 18:18:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Resumo Aulas - Gabrielly Sanches</title>
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="tela-azul"> 
+        <h1>SCRUM - RESUMO DAS AULAS</h1>
+        <h2><u>SCRUM:</u> Framework</h2>
+        <p>
+            É auto-gerenciável, ninguém comanda. Todos tem autonomia pra tomar decisão e existe um alinhamento entre todos.
+        </p>
+        <p>
+            <b><u>- Product Owner:</u></b> Maximiza a Entrega de Valor. Ordenar, analisar as ideias, faz o backlog. Prioriza e gerencia o Backlog do Produto.
+        </p>
+
+        <img src="/imgs/scrum_framework.jpg" alt="scrum-framework">
+
+    </div> 
+    
+</body>
+</html>
